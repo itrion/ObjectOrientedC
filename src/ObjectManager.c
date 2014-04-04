b1cea1cbdb73aa53a79cbb99fe6eb90c307e40a1
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-04 13:32:58 +0100

    ObjectManager now can create and delete objects

--- src/ObjectManager.c
+++ src/ObjectManager.c
@@ -1,10 +1,28 @@
 #include "ObjectManager.h"
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include "assert.h"
+#include "Class.h"
 
-void* new(const void* type, ...){
-	return malloc((const size_t) type);
+void* new(const void* objectClass, ...){
+	const struct Class* class = objectClass;
+	void* object = malloc(class->size);
+	assert(object);
+	*(const struct Class**)object = class;
+	va_list ap;
+	va_start(ap, objectClass);
+	object = class->constructor(object, &ap);
+	va_end(ap);
+	return object;
 }
 
-bool equals(const void* objectA, const void* objectB){
-	return false;
+void delete(const void* object){
+	const struct Class** class = object;
+	object = (*class)->destructor(object);
+	free((void*) object);
+}
+
+int compare(const void* objectA, const void* objectB){
+	const struct Class* const* class = objectA;
+	return (*class)->compare(objectA, objectB);
 }
--- src/ObjectManager.h
+++ src/ObjectManager.h
@@ -1,8 +1,8 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
-#include <stdbool.h>
 
-void* new(const void* descriptor, ...);
-bool equals(const void* objectA, const void* objectB);
+void* new(const void* clazz, ...);
+void delete(const void* object);
+int compare(const void* objectA, const void* objectB);
 
 #endif
