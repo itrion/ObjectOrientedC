00646773464b117c62f1c0558699aca88836b8ce
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-04-04 09:35:48 +0100

    C can`t overload methods it is necessary to work with objects

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,8 @@
 #include "Calculator.h"
 #include "UnitTest.h"
-#include "MemoryManager.h"
 
 int main(int argc, char const *argv[]){
 	assertEquals(2, sum(1, 1));
+	assertEquals(0.2, sum(0.1, 0.1));
 	return 0;
 }
--- src/UnitTest.h
+++ src/UnitTest.h
@@ -2,5 +2,6 @@
 #define UNIT_TEST_H
 
 void assertEquals(int expected, int actual);
+void assertEquals(double expected, double actual);
 
 #endif
